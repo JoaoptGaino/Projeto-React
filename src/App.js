9cfefbf30356f298d1bb683c06ce835e4267c8fb
João Pedro Theodoro Gaino <joao.gaino@eurekalabs.com.br>
2021-05-28 15:40:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { Grid } from "@material-ui/core";
+import Produto from "./components/Produto";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Grid item xs={12} xl={3} sm={4} md={6}>
+        <Produto
+          nome="Produto numero 1"
+          descricao="Descricao props numero 1"
+          preco={500}
+          img="https://images7.kabum.com.br/produtos/fotos/124417/notebook-acer-aspire-3-intel-core-i3-1005g1-4gb-1tb-windows-10-home-15-6-gray-a315-56-36z1_1599224319_g.jpg"
+        />
+      </Grid>
+      <Grid item xs={12} xl={3} sm={4} md={6}>
+        <Produto
+          nome="Produto com nome diferente"
+          descricao="Descricao diferente"
+          preco={120}
+          img="https://images7.kabum.com.br/produtos/fotos/124417/notebook-acer-aspire-3-intel-core-i3-1005g1-4gb-1tb-windows-10-home-15-6-gray-a315-56-36z1_1599224319_g.jpg"
+        />
+      </Grid>
+      <Grid item xs={12} xl={3} sm={4} md={6}>
+        <Produto
+          nome="Produto com nome diferente"
+          descricao="Descricao diferente"
+          preco={120}
+          img="https://images7.kabum.com.br/produtos/fotos/124417/notebook-acer-aspire-3-intel-core-i3-1005g1-4gb-1tb-windows-10-home-15-6-gray-a315-56-36z1_1599224319_g.jpg"
+        />
+      </Grid>
+      <Grid item xs={12} xl={3} sm={4} md={6}>
+        <Produto
+          nome="Produto com nome diferente"
+          descricao="Descricao diferente"
+          preco={120}
+          img="https://images7.kabum.com.br/produtos/fotos/124417/notebook-acer-aspire-3-intel-core-i3-1005g1-4gb-1tb-windows-10-home-15-6-gray-a315-56-36z1_1599224319_g.jpg"
+        />
+      </Grid>
+      <Grid item xs={12} xl={3} sm={4} md={6}>
+        <Produto
+          nome="Produto com nome diferente"
+          descricao="Descricao diferente"
+          preco={120}
+          img="https://images7.kabum.com.br/produtos/fotos/124417/notebook-acer-aspire-3-intel-core-i3-1005g1-4gb-1tb-windows-10-home-15-6-gray-a315-56-36z1_1599224319_g.jpg"
+        />
+      </Grid>
+      <Grid item xs={12} xl={3} sm={4} md={6}>
+        <Produto
+          nome="Produto com nome diferente"
+          descricao="Descricao diferente"
+          preco={120}
+          img="https://images7.kabum.com.br/produtos/fotos/124417/notebook-acer-aspire-3-intel-core-i3-1005g1-4gb-1tb-windows-10-home-15-6-gray-a315-56-36z1_1599224319_g.jpg"
+        />
+      </Grid>
+    </Grid>
   );
 }
 
